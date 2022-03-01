64cbf37367598845387246a5f2e9c12f5d2b327f
emilHof <95590295+emilHof@users.noreply.github.com>
2022-03-01 13:00:08 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,6 +1,28 @@
 from digi.xbee.devices import XBeeDevice
-xbee = XBeeDevice("/dev/ttyUSB2", 9600)
+import time
 
+
+
+def listen(self):
+    print("listening...")
+    data = self.read_data()
+    message = data
+    if message != None:
+        print("found a message")
+        return message.data.decode
+    else:
+        print("no message found")
+        print("sleeping...")
+        time.sleep(3)
+        return self.listen()
+
+
+xbee = XBeeDevice("/dev/ttyUSB2", 9600)
 xbee.open()
 xbee.send_data_broadcast("hello world")
+
+msg = xbee.listen()
 xbee.close()
+
+
+print(msg)
