5dc83883f99ed3c70b75b60d0dcaf4c912c69aee
emilHof <95590295+emilHof@users.noreply.github.com>
2022-04-04 16:56:33 -0400

    set up the main file to get ready for alpha testing

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
+import asyncio
+
 import config.config as config
-from interfaces.radio import RadioInterface
-from interfaces.gps import GPSInterface
+from interfaces.rfid import RFIDInterface
 from handlers.message_handler import MessageHandler
 
 radio_settings = {
-    "port": "/dev/ttyUSB2",
+    "port": "/dev/ttyUSB3",
     "rate": 9600,
 }
 
 gps_settings = {
-    "port": "/dev/ttyUSB4",
+    "port": "/dev/ttyUSB0",
     "rate": 9600,
 }
-config.set_config(radio_settings, gps_settings)
-handler = MessageHandler()
-xbee = RadioInterface()
-gps = GPSInterface()
 
-xbee.print_settings()
-# gps.print_settings()
-# gps.print_basics()
-
-message = xbee.listen()
-if message.startswith("@"):
-    err = handler.handle_message(message)
-    if err is not None:
-        print(err)
-else:
-    print("message was not handled!")
-    location = gps.get_location()
-    print("location:", location)
-    print("message:", message)
-    xbee.send_back(message + location)
+db_setting = {
+    "file": "local_data.db"
+}
 
 
+async def main():
+    config.set_config(radio_dict=radio_settings, gps_dict=gps_settings, db_dict=db_setting)
+    config.set_specific("rfid", "port", "/dev/ttyACM0")
+    config.set_specific("rfid", "rate", 9600)
+
+    rfid = RFIDInterface(gps=True)
+    msg_handler = MessageHandler(gps=True, radio=True)
+
+    # set up an asynchronous loop to propagate messages and listen for rfid signals
+    await asyncio.gather(
+        msg_handler.propagate_message(),
+        rfid.check_rfid("turb"),
+    )
+
+    return "all async loops exited"
+
+
+if __name__ == "__main__":
+    output = asyncio.run(main())
+    print(output)
