d1fab1813dedc337e7cc94dc9a296c7d6f1832b7
emilHof <95590295+emilHof@users.noreply.github.com>
2022-04-20 13:15:22 -0400

    added the new radio methods to the radio.py

--- interfaces/radio.py
+++ interfaces/radio.py
@@ -1,4 +1,6 @@
 import digi.xbee.devices as devices
+import digi.xbee.exception
+
 import config.config as config
 import asyncio
 
@@ -18,12 +20,18 @@ def add_hash(out_msg: str, time: str) -> str:
 class RadioInterface:
     """ __init__ is called on initialization of every new RadioInterface """
 
-    def __init__(self, in_queue: asyncio.Queue, dep_queue: asyncio.Queue, debug: bool = False):
+    def __init__(
+            self, in_queue: asyncio.Queue,
+            dep_queue: asyncio.Queue,
+            close_chan: asyncio.Queue,
+            debug: bool = False
+    ):
         radio = config.config["radio"]  # gets the radio parameters from the config file
         self.port, self.rate = radio["port"], radio["rate"]  # initializes them as attributes
         self.xbee = devices.XBeeDevice(self.port, self.rate)  # creates a new xbee device as a RadioInterface attribute
         self.in_queue = in_queue
         self.dep_queue = dep_queue
+        self.close_chan = close_chan
         if debug:
             print("xbee created!")
 
@@ -66,6 +74,47 @@ class RadioInterface:
             xbee.open()
             xbee.send_data_broadcast(out_msg)  # broadcast the msg
             xbee.close()
+
+            self.dep_queue.task_done()  # mark the msg as sent
+
+            await asyncio.sleep(sleep_time)  # sleep for the indicated time
+
+    def __send_callback(self, msg):
+        self.in_queue.put_nowait(msg.data.decode("utf8"))
+
+    def __register_callback(self):
+        self.xbee.add_data_received_callback(self.__send_callback)
+
+    def __deregister_callback(self):
+        self.xbee.del_data_received_callback(self.__send_callback)
+
+    async def radio_open(self):
+        self.xbee.open()
+
+        self.__register_callback()
+
+        await self.close_chan.get()
+
+        self.__deregister_callback()
+
+        self.xbee.close()
+
+    async def sender(self, debug: bool = False):
+
+        while True:
+            task = await self.dep_queue.get()  # get task from the dep_queue
+
+            out_msg, sleep_time, time = task[0], task[1], task[2]  # get the msg and sleep time from the task item
+
+            if time is not None:  # check if there was any time passed with the msg
+                out_msg = add_hash(out_msg, time)
+
+            if debug: print(f'sent message: {out_msg}')
+
+            try:
+                self.xbee.send_data_broadcast(out_msg)  # broadcast the msg
+            except digi.xbee.exception.TransmitException as error:
+                print(error)
 
             self.dep_queue.task_done()  # mark the msg as sent
 
