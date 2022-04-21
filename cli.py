a8ddb91fd0ccbb194b188f8bcf4fc8e0a2ec1e56
emilHof <95590295+emilHof@users.noreply.github.com>
2022-04-20 22:06:18 -0400

    added rfid to cli

--- cli.py
+++ cli.py
@@ -2,6 +2,7 @@ import asyncio
 
 import config.config as config
 from interfaces.radio import RadioInterface
+from interfaces.rfid import RFIDInterface
 from pkg.handlers.message_handler import MessageHandler
 from pkg.ui.cli import RadioCLI
 
@@ -22,7 +23,9 @@ db_setting = {
 
 async def main():
     config.set_config(radio_dict=radio_settings, gps_dict=gps_settings, db_dict=db_setting)
-    config.set_specific("NI", "ni", "01")
+    config.set_specific("rfid", "port", "/dev/ttyACM0")  # make sure this works
+    config.set_specific("rfid", "rate", 9600)  # make sure this works
+    config.set_specific("NI", "ni", "02")
 
     in_queue = asyncio.Queue()
     dep_queue = asyncio.Queue()
@@ -30,12 +33,14 @@ async def main():
     close_chan = asyncio.Queue()
 
     radio = RadioInterface(in_queue, dep_queue, close_chan)
+    rfid = RFIDInterface(gps=False)  # make sure this works
     msg_handler = MessageHandler(in_queue, dep_queue, print_queue, gps=False)
     cli = RadioCLI(dep_queue, print_queue)
 
     # set up an asynchronous loop to propagate messages and listen for rfid signals
     await asyncio.gather(
         msg_handler.handle_msg(),
+        rfid.check_rfid("temp"),  # make sure this works
         radio.radio_open(),
         radio.sender(),
         cli.cli()
